b2444a0e76cae2b9c46ab734bbe3b712a1707f6a
Per Buer <per.buer@gmail.com>
2025-10-19 09:14:32 +0200

    refactor: simplify CDB implementation by reusing `getFromBytes` and `allFromBytes`

--- comparison/go.mod
+++ comparison/go.mod
@@ -2,4 +2,11 @@ module cdb-comparison
 
 go 1.25.0
 
-require github.com/colinmarc/cdb v0.0.0-20190223170904-60f317823f70 // indirect
+require github.com/perbu/cdb v0.0.0
+
+require (
+	github.com/colinmarc/cdb v0.0.0-20190223170904-60f317823f70 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+)
+
+replace github.com/perbu/cdb => ../
--- mmap.go
+++ mmap.go
@@ -1,8 +1,6 @@
 package cdb
 
 import (
-	"bytes"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"iter"
@@ -60,38 +58,7 @@ func Mmap(file *os.File) (*MmapCDB, error) {
 
 // Get returns the value for a given key using memory-mapped access.
 func (cdb *MmapCDB) Get(key []byte) ([]byte, error) {
-	hash := cdbHash(key)
-
-	table := readTableAt(cdb.data, uint8(hash&0xff))
-	if table.length == 0 {
-		return nil, nil
-	}
-
-	// Probe the given hash table, starting at the given slot.
-	startingSlot := (uint64(hash) >> 8) % table.length
-	slot := startingSlot
-
-	for {
-		slotOffset := table.offset + (16 * slot)
-		slotHash, offset := readTupleMmap(cdb.data, slotOffset)
-
-		// An empty slot means the key doesn't exist.
-		if slotHash == 0 {
-			break
-		} else if slotHash == uint64(hash) {
-			value := getValueAt(cdb.data, offset, key)
-			if value != nil {
-				return value, nil
-			}
-		}
-
-		slot = (slot + 1) % table.length
-		if slot == startingSlot {
-			break
-		}
-	}
-
-	return nil, nil
+	return getFromBytes(cdb.data, key)
 }
 
 // Close unmaps the file and closes the file descriptor.
@@ -106,8 +73,8 @@ func (cdb *MmapCDB) Close() error {
 		cdb.data = nil
 	}
 	if cdb.file != nil {
-		if err := cdb.file.Close(); err == nil {
-			errs = append(errs, err)
+		if err := cdb.file.Close(); err != nil {
+			errs = append(errs, fmt.Errorf("file.Close: %w", err))
 		}
 		cdb.file = nil
 	}
@@ -138,38 +105,7 @@ func NewInMemory(data []byte) (*InMemoryCDB, error) {
 
 // Get returns the value for a given key from the in-memory CDB.
 func (cdb *InMemoryCDB) Get(key []byte) ([]byte, error) {
-	hash := cdbHash(key)
-
-	table := readTableAt(cdb.data, uint8(hash&0xff))
-	if table.length == 0 {
-		return nil, nil
-	}
-
-	// Probe the given hash table, starting at the given slot.
-	startingSlot := (uint64(hash) >> 8) % table.length
-	slot := startingSlot
-
-	for {
-		slotOffset := table.offset + (16 * slot)
-		slotHash, offset := readTupleMmap(cdb.data, slotOffset)
-
-		// An empty slot means the key doesn't exist.
-		if slotHash == 0 {
-			break
-		} else if slotHash == uint64(hash) {
-			value := getValueAt(cdb.data, offset, key)
-			if value != nil {
-				return value, nil
-			}
-		}
-
-		slot = (slot + 1) % table.length
-		if slot == startingSlot {
-			break
-		}
-	}
-
-	return nil, nil
+	return getFromBytes(cdb.data, key)
 }
 
 // Close is a no-op for InMemoryCDB since there are no resources to release.
@@ -185,57 +121,7 @@ func (cdb *InMemoryCDB) Size() int {
 
 // All returns an iterator over all key-value pairs in the database.
 func (cdb *InMemoryCDB) All() iter.Seq2[[]byte, []byte] {
-	return func(yield func([]byte, []byte) bool) {
-		// Find the minimum table offset to determine where data section ends
-		var endPos uint64
-		endPos = uint64(len(cdb.data)) // Start with file size, then find minimum table offset
-
-		for i := 0; i < 256; i++ {
-			table := readTableAt(cdb.data, uint8(i))
-			if table.length > 0 && table.offset < endPos {
-				endPos = table.offset
-			}
-		}
-
-		// If no hash tables exist, data goes to end of file
-		if endPos == uint64(len(cdb.data)) {
-			// For empty database, endPos should be indexSize
-			if endPos == uint64(indexSize) {
-				endPos = uint64(indexSize)
-			}
-		}
-
-		pos := uint64(indexSize)
-		for pos < endPos {
-			// Ensure we don't read past the end of data
-			if int(pos)+16 > len(cdb.data) {
-				return
-			}
-
-			keyLength, valueLength := readTupleMmap(cdb.data, pos)
-
-			// Calculate total record size and check bounds
-			totalSize := 16 + keyLength + valueLength
-			if int(pos+totalSize) > len(cdb.data) {
-				return
-			}
-
-			// Extract key and value directly from data
-			dataStart := int(pos + 16)
-			keyEnd := dataStart + int(keyLength)
-			valueEnd := keyEnd + int(valueLength)
-
-			key := cdb.data[dataStart:keyEnd]
-			value := cdb.data[keyEnd:valueEnd]
-
-			// Yield the key-value pair
-			if !yield(key, value) {
-				return // Early termination requested
-			}
-
-			pos += totalSize
-		}
-	}
+	return allFromBytes(cdb.data)
 }
 
 // Keys returns an iterator over all keys in the database.
@@ -260,55 +146,6 @@ func (cdb *InMemoryCDB) Values() iter.Seq[[]byte] {
 	}
 }
 
-// readTupleMmap reads a 64-bit tuple from memory-mapped data.
-func readTupleMmap(data []byte, offset uint64) (uint64, uint64) {
-	if int(offset)+16 > len(data) {
-		return 0, 0
-	}
-	first := binary.LittleEndian.Uint64(data[offset : offset+8])
-	second := binary.LittleEndian.Uint64(data[offset+8 : offset+16])
-	return first, second
-}
-
-// readTableAt reads a table entry from the data at the given table number.
-func readTableAt(data []byte, tableNum uint8) table {
-	off := int(tableNum) * 16
-	return table{
-		offset: binary.LittleEndian.Uint64(data[off : off+8]),
-		length: binary.LittleEndian.Uint64(data[off+8 : off+16]),
-	}
-}
-
-// getValueAt retrieves a value at the given offset from the data.
-func getValueAt(data []byte, offset uint64, expectedKey []byte) []byte {
-	if int(offset)+16 > len(data) {
-		return nil
-	}
-
-	keyLength, valueLength := readTupleMmap(data, offset)
-
-	// We can compare key lengths before reading the key at all.
-	if int(keyLength) != len(expectedKey) {
-		return nil
-	}
-
-	dataStart := int(offset + 16)
-	dataEnd := dataStart + int(keyLength+valueLength)
-	if dataEnd > len(data) {
-		return nil
-	}
-
-	keyEnd := dataStart + int(keyLength)
-	key := data[dataStart:keyEnd]
-
-	// If the keys don't match, this isn't it.
-	if !bytes.Equal(key, expectedKey) {
-		return nil
-	}
-
-	return data[keyEnd:dataEnd]
-}
-
 // Size returns the size of the memory-mapped data.
 func (cdb *MmapCDB) Size() int {
 	return len(cdb.data)
@@ -316,57 +153,7 @@ func (cdb *MmapCDB) Size() int {
 
 // All returns an iterator over all key-value pairs in the database.
 func (cdb *MmapCDB) All() iter.Seq2[[]byte, []byte] {
-	return func(yield func([]byte, []byte) bool) {
-		// Find the minimum table offset to determine where data section ends
-		var endPos uint64
-		endPos = uint64(len(cdb.data)) // Start with file size, then find minimum table offset
-
-		for i := 0; i < 256; i++ {
-			table := readTableAt(cdb.data, uint8(i))
-			if table.length > 0 && table.offset < endPos {
-				endPos = table.offset
-			}
-		}
-
-		// If no hash tables exist, data goes to end of file
-		if endPos == uint64(len(cdb.data)) {
-			// For empty database, endPos should be indexSize
-			if endPos == uint64(indexSize) {
-				endPos = uint64(indexSize)
-			}
-		}
-
-		pos := uint64(indexSize)
-		for pos < endPos {
-			// Ensure we don't read past the end of mapped data
-			if int(pos)+16 > len(cdb.data) {
-				return
-			}
-
-			keyLength, valueLength := readTupleMmap(cdb.data, pos)
-
-			// Calculate total record size and check bounds
-			totalSize := 16 + keyLength + valueLength
-			if int(pos+totalSize) > len(cdb.data) {
-				return
-			}
-
-			// Extract key and value directly from mmap data
-			dataStart := int(pos + 16)
-			keyEnd := dataStart + int(keyLength)
-			valueEnd := keyEnd + int(valueLength)
-
-			key := cdb.data[dataStart:keyEnd]
-			value := cdb.data[keyEnd:valueEnd]
-
-			// Yield the key-value pair
-			if !yield(key, value) {
-				return // Early termination requested
-			}
-
-			pos += totalSize
-		}
-	}
+	return allFromBytes(cdb.data)
 }
 
 // Keys returns an iterator over all keys in the database.
